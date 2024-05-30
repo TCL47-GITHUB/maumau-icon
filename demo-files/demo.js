45e4e2d7266de59bfb64be80e118524f87470ad7
Trương Công Lý <55310194+TCL47-GITHUB@users.noreply.github.com>
2024-05-30 11:17:23 +0700

    Add files via upload

--- demo-files/demo.js
+++ demo-files/demo.js
@@ -91,7 +91,7 @@ document.body.addEventListener("click", function(e) {
         overlay.style.display = 'flex';
 
         // Danh sách icon mới
-        var newIcons = ["mm-shopping-bag","mm-shopping-bag-solid","mm-checklist","mm-checklist-solid","mm-bell","mm-bell-solid","mm-chats-1-smile","mm-chats-1-smile-solid","mm-home","mm-home-solid","mm-post-add","mm-car-plane","mm-chats-1-smile","mm-chats-dots-thin","mm-chats-2","mm-chats-thin","mm-chats-1","mm-chats","mm-more-h-1","mm-check-list-file-thin","mm-feedback2","mm-clipboard-list-check","mm-shopping-cart-3","mm-shopping-cart-2","mm-shopping-cart-thin","mm-shopping-cart","mm-shopping-cart-1"
+        var newIcons = ["mm-avatar","mm-avatar-solid","mm-shopping-bag","mm-shopping-bag-solid","mm-checklist","mm-checklist-solid","mm-bell","mm-bell-solid","mm-chats-1-smile","mm-chats-1-smile-solid","mm-home","mm-home-solid","mm-post-add","mm-car-plane","mm-chats-1-smile","mm-chats-dots-thin","mm-chats-2","mm-chats-thin","mm-chats-1","mm-chats","mm-more-h-1","mm-check-list-file-thin","mm-feedback2","mm-clipboard-list-check","mm-shopping-cart-3","mm-shopping-cart-2","mm-shopping-cart-thin","mm-shopping-cart","mm-shopping-cart-1"
 		];
 
         // Hiển thị icon mới trong modal
